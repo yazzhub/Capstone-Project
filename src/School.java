47a87756e74b510db57e3db733208254ce4950cd
Yasmin <yasmin@Yasmins-MacBook-Air-2.local>
2022-11-17 20:45:01 -0600

    Welcome page, crud student/teacher started

--- src/School.java
+++ src/School.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Scanner;
 
 /**
  * Manages all students/teachers
@@ -11,6 +12,8 @@ public class School {
     private HashMap<Integer,Student> students;
     private int totalMoneyEarned;
     private int totalMoneySpent;
+
+    Scanner scan = new Scanner (System.in);
 
     /**
      * new school object is created
@@ -55,4 +58,85 @@ public class School {
     public void setTotalMoneySpent(int totalMoneySpent) {
         this.totalMoneySpent = totalMoneySpent;
     }
-}
+
+    public void welcomePage() {
+        String message = "Welcome to Miss Yasmin's School for Gifted Youth 🦸🏽‍ \n You are now using the school management software \n" +
+                "Would you like to load a Student or Teacher manager page? (Type 'S' for students, 'T' for teachers Type 'N' to begin a new project)";
+        System.out.println(message);
+        String welcomereply = scan.nextLine();
+        if(welcomereply.equalsIgnoreCase("S")) {
+            //take to student load data
+
+        } else if (welcomereply.equalsIgnoreCase("T"))    {
+            //take to teacher load data
+            } else if (welcomereply.equalsIgnoreCase("N")) {
+            // take to crudSelector
+        }
+
+        }
+
+        public void newProject () {
+
+        String newPro = "What you like to create? A new student or Teacher? Type '1' for Student or '2' for Teacher";
+        System.out.println(newPro);
+        String projectReply = scan.nextLine();
+
+        switch (projectReply) {
+            case 1:
+            crudStudent();
+                break;
+            case 2:
+            //teacher crud menu
+                break;
+        }
+    }
+
+    public void crudStudent() {
+        String crudStudentMessage = "You've selected the Student System! Here are your options:" +
+                          "\n Input '1' to create a new student" +
+                          "\n Input '2' to read all the students" +
+                          "\n Input '3' to update a student" +
+                          "\n Input '4' to delete a student" +
+                          "\n Input '*' to quit menu" ;
+
+       String crudStudentreply = scan.nextLine();
+
+        switch (crudStudentreply) {
+
+            case 1:
+            //create student
+                break;
+            case 2:
+            //read student
+                break;
+            case 3:
+            //update student
+                break;
+            case 4:
+            //delete student
+                break;
+            case 5:
+            //quit crud
+
+        }
+    }
+    public void crudTeacher() {
+
+        String crudTeacherMessage = "You've selected the Teacher System! Here are your options:" +
+                "\n Input '1' to create a new teacher" +
+                "\n Input '2' to read all the teachers" +
+                "\n Input '3' to update a teacher" +
+                "\n Input '4' to delete a teacher" +
+                "\n Input '*' to quit menu" ;
+
+        System.out.println(crudTeacherMessage);
+
+        String crudTeacherReply = scan.nextLine();
+
+        switch (crudTeacherReply) {
+
+            case 1:
+
+        }
+    }
+    }
